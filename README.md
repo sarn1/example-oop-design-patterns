c8c41f933af61aa9f01a497f02d458af7b617738
sarn1 <sarn@tyndale.com>
2017-06-20 17:59:33 -0500

    notes integration

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
-# [OOP Design Patterns by Source Making][published url]
+# OOP Design Patterns
+Design patterns examples and notes from the following sites:
+* [Source Making][sourcemaking url]
+* [OODesign][oodesign url]
+* [TreeHouse: Introduction to Design Patterns ][treehouse url]
+* [Tuts+: PHP Design Patterns][tutsplus url]
+* [Tuts+ Github OOD Page][tutsplusood url]
 
-Design patterns from Source Making.
+[sourcemaking url]: https://sourcemaking.com
+[oodesign url]: http://www.oodesign.com/
+[treehouse url]: https://teamtreehouse.com/library/introduction-to-design-patterns
+[tutsplus url]: https://code.tutsplus.com/courses/php-design-patterns
+[tutsplusood url]: https://github.com/tutsplus/php-design-patterns
 
-## Source Files Description
-
-This repository contains the source for sample PHP code for each design pattern that is on the website.
-
-[published url]: https://sourcemaking.com
+### Other Notes
+* Pattern design is not a magic bullet and will not solve all programming problems.
+* A developer should NOT start with a pattern, instead reach for it only if you need it.
+* In other words, always go for the simpliest solution first and use a design pattern if you need it.
+* The benefit of the familiarity of design patterns allows shared vocabulary and allows one to communicate very specific and complex solutions with just a few words.
+* MVC = complex pattern because it follows several design patterns.  (controller = mediator, view = composite, model = observer)
+* Patterns allows framework authors to structure their code in a way that is predicatable and understandble by developers looking to integrate/extend their framework without the developer modifying the framework code.
+* Patterns allow you to leverage "experence" re-use.  Simular to code re-use.  Patterns are tried and true ways for structuring and organizing code by those who've been there before.
+* Patterns may take more time to develop, it creates more complexity, and may contain more files, but they allow for easier understanding/comprehension of the codebase, ease maintenance, and allow for easier adaptability and tweaking in a fast pace environment.
+* When describing a design pattern:
+    * pattern name
+    * problem/intent = when to apply it, may contain list of conditions
+    * solution/implementation = describe the design, relationship, responsibility, - contains diagrame, high-level
+    * consequences = take more time, make more complexity, contains more files
+* Don't worry too much about patterns or doing something "correctly" while writing your code.  That's the easiest and quickest way to bog you down to the point where you get no work done.  Instead, write the application so it runs. When it runs, then you can find spots to refactor to make it better.  Start small and find spots where you could apply a pattern.  When you do find a spot to clean up, applying patterns will become easier.
---
+++ sourcemaking/structural-bridge.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+https://sourcemaking.com/design_patterns/bridge/php
+
+In the Bridge Design Pattern, functionality abstraction and implementation are in 
+separate class hierarchies.
+
+In this example we have BridgeBook which uses either BridgeBookCapsImp or  BridgeBookStarsImp. 
+BridgeBook will assign one implementation or the other each time  BridgeBook is instantiated.
+
+The bridge pattern is helpful when you want to decouple a class from it's implementation.
+*/
+
+abstract class BridgeBook {     
+    private $bbAuthor;
+    private $bbTitle;
+    private $bbImp;
+    function __construct($author_in, $title_in, $choice_in) {
+      $this->bbAuthor = $author_in;
+      $this->bbTitle  = $title_in;
+      if ('STARS' == $choice_in) {
+        $this->bbImp = new BridgeBookStarsImp();
+      } else {
+        $this->bbImp = new BridgeBookCapsImp();
+      }
+    }    
+    function showAuthor() {
+      return $this->bbImp->showAuthor($this->bbAuthor);
+    }
+    function showTitle() {
+      return $this->bbImp->showTitle($this->bbTitle);
+    }
+}
+ 
+class BridgeBookAuthorTitle extends BridgeBook {    
+    function showAuthorTitle() {
+      return $this->showAuthor() . "'s " . $this->showTitle();
+    }
+}  
+ 
+class BridgeBookTitleAuthor extends BridgeBook {    
+    function showTitleAuthor() {
+      return $this->showTitle() . ' by ' . $this->showAuthor();
+    }
+}
+ 
+abstract class BridgeBookImp {    
+    abstract function showAuthor($author);
+    abstract function showTitle($title);
+}
+
+class BridgeBookCapsImp extends BridgeBookImp {    
+    function showAuthor($author_in) {
+      return strtoupper($author_in); 
+    }
+    function showTitle($title_in) {
+      return strtoupper($title_in); 
+    }
+}
+
+class BridgeBookStarsImp extends BridgeBookImp {    
+    function showAuthor($author_in) {
+      return Str_replace(" ","*",$author_in); 
+    }
+    function showTitle($title_in) {
+      return Str_replace(" ","*",$title_in); 
+    }
+}
+
+  writeln('BEGIN TESTING BRIDGE PATTERN');
+  writeln('');
+ 
+  writeln('test 1 - author title with caps');
+  $book = new BridgeBookAuthorTitle('Larry Truett','PHP for Cats','CAPS');
+  writeln($book->showAuthorTitle());
+  writeln('');
+
+  writeln('test 2 - author title with stars');
+  $book = new BridgeBookAuthorTitle('Larry Truett','PHP for Cats','STARS');
+  writeln($book->showAuthorTitle());
+  writeln('');
+
+  writeln('test 3 - title author with caps');
+  $book = new BridgeBookTitleAuthor('Larry Truett','PHP for Cats','CAPS');
+  writeln($book->showTitleAuthor());
+  writeln('');
+
+  writeln('test 4 - title author with stars');
+  $book = new BridgeBookTitleAuthor('Larry Truett','PHP for Cats','STARS');
+  writeln($book->showTitleAuthor());
+  writeln('');
+
+  writeln('END TESTING BRIDGE PATTERN');
+
+  function writeln($line_in) {
+    echo $line_in."<br/>";
+  }
+
+?>
+Output
+
+BEGIN TESTING BRIDGE PATTERN
+
+test 1 - author title with caps
+LARRY TRUETT's PHP FOR CATS
+
+test 2 - author title with stars
+Larry*Truett's PHP*for*Cats
+
+test 3 - title author with caps
+PHP FOR CATS by LARRY TRUETT
+
+test 4 - title author with stars
+PHP*for*Cats by Larry*Truett
+
+END TESTING BRIDGE PATTERN
